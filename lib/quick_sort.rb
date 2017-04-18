a66f232c32f08d3b3048dab879b883e501984416
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-04-18 10:13:36 -0700

    Complete specs

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -14,32 +14,31 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
-    return array if array.length < 2
-    pivot = self.partition(array, start, length, &prc)
-    array[0...pivot] = self.sort2!(array[0...pivot], &prc)
-    array[(pivot + 1)..-1] = self.sort2!(array[(pivot + 1)..-1], &prc)
-    return array[0...pivot] + [array[pivot]] + array[(pivot + 1)..-1]
+    prc ||= Proc.new {|a,b| a <=> b}
+    return [] if length < 1
+    pivot_idx = self.partition(array, start, length, &prc)
+    left = pivot_idx - start
+    right = length - (left + 1)
+
+    self.sort2!(array, start, left, &prc)
+    self.sort2!(array, pivot_idx + 1, right, &prc)
+    array
   end
 
   def self.partition(array, start, length, &prc)
     prc ||= Proc.new{|a,b| a <=> b}
     pivot = array[start]
-    i = start
-    b_index = start + 1
+    pivot_idx = start
+    i = start + 1
     while i < start + length
-      if array[i] < pivot
-        prc.call(array[i], pivot) != -1
-        if i != b_index
-          array[i], array[b_index] = array[b_index], array[i]
-          # prc.call(array[i], pivot)
-        end
-        b_index += 1
+      if prc.call(pivot, array[i]) == 1
+        array[i], array[pivot_idx + 1] = array[pivot_idx + 1], array[i]
+        pivot_idx += 1
       end
       i += 1
     end
-    prc.call(array[i], pivot)
-    array[start], array[b_index - 1] = array[b_index - 1], array[start]
 
-    b_index - 1
+    array[start], array[pivot_idx] = array[pivot_idx], array[start]
+    return pivot_idx
   end
 end
